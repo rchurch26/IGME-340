ae7dc6d0a42aa631a4614018c715b3d8fee8b3ed
rchurch26 <rahmelchurch@gmail.com>
2024-03-02 18:17:27 -0500

    Created logic to finding gifs

---
+++ Assignments/Gif Finder/gif_finder_logic/bin/gif_finder_logic.dart
@@ -0,0 +1,5 @@
+import 'package:gif_finder_logic/gif_finder_logic.dart' as gif_finder_logic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gif_finder_logic.calculate()}!');
+}
--- Assignments/Gif Finder/gif_finder_logic/test/gif_finder_logic_test.dart
+++ Assignments/Gif Finder/gif_finder_logic/test/gif_finder_logic_test.dart
@@ -1,31 +1,37 @@
+import 'package:test/test.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 String userSearch = "sports";
-List<dynamic> posts = [];
+Map<String, dynamic> posts = {};
 
 void main() {
-  grabPosts();
-  for(int i = 0; i < posts.length; i++)
+  test( 'API Test', () async
   {
-    print("${posts[i][9]}: ${posts[i][2]}");
+    await grabPosts();
+    for(int i = 0; i < posts["data"].length; i++)
+    {
+      print("${posts["data"][i]["title"]}: ${posts["data"][i]["url"]}");
+    }
   }
+  );
 }
 
-void grabPosts() async
+Future grabPosts() async
 {
   posts = await goGetPosts();
 }
 
-Future<List> goGetPosts() async
+Future<dynamic> goGetPosts() async
 {
   String apiPostUrl = "https://api.giphy.com/v1/gifs/search?api_key=aKj4vQZcn3ME5T0ND8ks5ANbTsb8z5v0&q=$userSearch&limit=25&offset=0&rating=pg-13&lang=en&bundle=messaging_non_clips";
-  var data = [];
+  var data = {};
   var response = await http.get(Uri.parse(apiPostUrl));
   if(response.statusCode == 200)
   {
     print("Got it");
     data = jsonDecode(response.body);
+    print(data.runtimeType);
   }
   else
   {
